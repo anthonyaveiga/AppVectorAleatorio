f57bb50ea2380fb400495ac37701da988c81a383
Anthony <anthony.aveiga@pucese.edu.ec>
2018-12-17 08:59:37 -0500

    commit

---
+++ AppVectorAleatorio/AppVectorAleatorio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVectorAleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[MAX];
+            leerVector(x)
+        }
+
+        public static void imprimirVector(int[] x)
+        {
+            for (int i = 0; i < MAX; i++) { }
+        }
+    }
+}
